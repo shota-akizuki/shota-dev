2313958aeafcf747c72491659e3ff2b83f38a2f5
Shota <64979430+shota-akizuki@users.noreply.github.com>
2021-03-23 02:51:30 +0900

    fix: Next.jsのビルドが通らない問題を修正

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,8 +1,8 @@
-export const getStaticProps = async (context) => {
+export default async function getStaticProps(context) {
   const slug = context.params?.slug;
   const draftKey = context.previewData?.draftKey;
   const content = await fetch(
-    `https://shota-akizuki.microcms.io/apis/blog/${slug}${
+    `https://shota-akizuki.microcms.io/api/v1/blog/${slug}${
       draftKey !== undefined ? `?draftKey=${draftKey}` : ''
     }`,
     { headers: { 'X-API-KEY': process.env.API_KEY || '' } }
@@ -12,4 +12,4 @@ export const getStaticProps = async (context) => {
       content
     }
   };
-};
+}
