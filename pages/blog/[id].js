56f23b31df41d7275f6876dc62b1467fd1886003
Shota <64979430+shota-akizuki@users.noreply.github.com>
2021-03-24 04:16:12 +0900

    fix code.

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -63,14 +63,18 @@ export const getStaticPaths = async () => {
 // データをテンプレートに受け渡す部分の処理
 export const getStaticProps = async (context) => {
   const id = context.params.id;
-
+  const draftKey = context.previewData.draftKey;
+  const preview = context.preview;
   const key = {
     headers: { 'X-API-KEY': process.env.API_KEY }
   };
-  const data = await fetch(
-    'https://shota-akizuki.microcms.io/api/v1/blog/' + id,
-    key
-  )
+
+  url = 'https://shota-akizuki.microcms.io/api/v1/blog/' + id;
+  if (preview) {
+    url += `?draftKey=${draftKey}`;
+  }
+
+  const data = await fetch(url, key)
     .then((res) => res.json())
     .catch(() => null);
   return {
