7037b37f04abf7f5063edabb4f17d7b094a5ae67
Shota <64979430+shota-akizuki@users.noreply.github.com>
2021-03-24 03:57:46 +0900

    Add blog preview.

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -62,9 +62,8 @@ export const getStaticPaths = async () => {
 
 // データをテンプレートに受け渡す部分の処理
 export const getStaticProps = async (context) => {
-  const id = context.params?.id;
-  const draftKey = context.previewData?.draftKey;
-  const preview = context.preveiw;
+  const id = context.params.id;
+
   const key = {
     headers: { 'X-API-KEY': process.env.API_KEY }
   };
@@ -74,15 +73,6 @@ export const getStaticProps = async (context) => {
   )
     .then((res) => res.json())
     .catch(() => null);
-  // 下書きは draftKey を含む必要があるのでプレビューの時は追加
-  if (preview) {
-    const data = await fetch(
-      `https://shota-akizuki.microcms.io/api/v1/blog/${id}${
-        draftKey !== undefined ? `?draftKey=${draftKey}` : ''
-      }`,
-      key
-    );
-  }
   return {
     props: {
       blog: data
