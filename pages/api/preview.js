2f763b4471316682e74e94cb95fa466dccee455e
Shota <64979430+shota-akizuki@users.noreply.github.com>
2021-03-25 04:12:22 +0900

    Add preview mode.

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,32 +1,27 @@
-import fetch from 'node-fetch';
-
 export default async (req, res) => {
-  // クエリの確認
-  if (req.query.secret !== process.env.SECRET_KEY || !req.query.slug) {
+  //次のパラメーターを確認。
+  if (!req.query.id) {
     return res.status(401).json({ message: 'Invalid token' });
   }
-
-  // 下書きのデータを取得
-  const content = await fetch(
-    `https://shota-akizuki.microcms.io/api/v1/blog/${req.query.id}?fields=id&draftKey=${req.query.draftKey}`,
+  // 提供された `slug` が存在しているかどうか確認するため、ヘッドレスCMSをフェッチ。
+  const post = await fetch(
+    `https://shota-akizuki.microcms.io/api/v1/blog/${req.query.id}?draftKey=${req.query.draftKey}`,
     { headers: { 'X-API-KEY': process.env.API_KEY || '' } }
   )
     .then((res) => res.json())
     .catch((error) => null);
 
-  // エラー処理
-
-  if (!content) {
+  // slugが存在しない場合、プレビューモードを有効にしないようにする、
+  if (!post) {
     return res.status(401).json({ message: 'Invalid slug' });
   }
-
+  // Cookiesを設定し、プレビューモードを有効にする
   // プレビューデータを格納
   res.setPreviewData({
-    slug: content.id,
+    id: req.query.id,
     draftKey: req.query.draftKey
   });
-
-  // // 詳細ページへリダイレクト
-  res.writeHead(307, { Location: `blog/${content.id}` });
+  // 詳細ページへリダイレクト
+  res.writeHead(307, { Location: `/blog/${req.query.id}` });
   res.end('Preview mode enabled');
 };
