3564c6f7f6e5e0a665c3f35e6cdc1d9bd640c892
Shota <64979430+shota-akizuki@users.noreply.github.com>
2021-03-27 04:34:30 +0900

    Add contacts.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Header from '../components/header';
 import styles from '../styles/home.module.css';
 import { Footer } from '../components/footer';
+import ExtLink from '../components/ext-link';
 
 export default function Home() {
   return (
@@ -10,12 +11,45 @@ export default function Home() {
         <h1>Shota Akizuki</h1>
         <p>
           I’m a developer, designer, and creator. I work at a company as a
-          Junior Developer. I enjoy working and crafting beautiful web sites.
+          Junior Developer in Osaka. I enjoy working and crafting beautiful web
+          sites.
         </p>
         <p>
           大阪でフロントエンドエンジニア・デザイナーをしています。Web制作・開発について何か僕にできることがあれば、気軽にご連絡ください。
         </p>
+        <hr className={styles.divider} />
+        <ul>
+          <li>
+            Twitter
+            <ExtLink href="https://twitter.com/shota_akizuki">
+              {' '}
+              @shota_akizuki
+            </ExtLink>
+          </li>
+          <li>
+            GitHub
+            <ExtLink href="https://github.com/shota-akizuki">
+              {' '}
+              @shota-akizuki
+            </ExtLink>
+          </li>
+          <li>
+            Mail
+            <ExtLink href="mailto:shota0609akizuki@gmail.com">
+              {' '}
+              @shota0609akizuki@gmail.com
+            </ExtLink>
+          </li>
+          <li>
+            Instagram
+            <ExtLink href="https://instagram.com/shota_akizuki">
+              {' '}
+              @shota_akizuki
+            </ExtLink>
+          </li>
+        </ul>
       </div>
+
       <Footer />
     </>
   );
