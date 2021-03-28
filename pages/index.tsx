9fa1c77f103b83a9ca9eb4d804a1907ff863e56c
Shota <64979430+shota-akizuki@users.noreply.github.com>
2021-03-29 02:46:12 +0900

    Add svgs.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,11 @@ import Header from '../components/header';
 import styles from '../styles/home.module.css';
 import { Footer } from '../components/footer';
 import ExtLink from '../components/ext-link';
+import Twitter from '../components/svgs/twitter';
+import GitHub from '../components/svgs/github';
+import Envelope from '../components/svgs/envelope';
+import Instagram from '../components/svgs/instagram';
+import React from 'react';
 
 export default function Home() {
   return (
@@ -10,8 +15,9 @@ export default function Home() {
       <div className={styles.main}>
         <h1>Shota Akizuki</h1>
         <p>
-          I’m a developer, designer, and creator. I work as a Junior Developer
-          in Osaka. I enjoy working and crafting beautiful web sites.
+          I’m a developer, designer, and creator. I work as a Developer in
+          Osaka. I enjoy working and crafting beautiful websites. Feel free to
+          contact with me.
         </p>
         <p>
           大阪でWebデザイナー・エンジニアをしています。Web制作・開発についてできることがあれば、お気軽にご相談ください。
@@ -19,28 +25,29 @@ export default function Home() {
         <p className={styles.divider} />
         <ul>
           <li>
-            Twitter
+            <Twitter style={{ fill: 'var(--color-accents-1)', height: 24 }} />
+
             <ExtLink href="https://twitter.com/shota_akizuki">
               {' '}
               @shota_akizuki
             </ExtLink>
           </li>
           <li>
-            GitHub
+            <GitHub style={{ fill: 'var(--color-accents-1)', height: 24 }} />
             <ExtLink href="https://github.com/shota-akizuki">
               {' '}
               @shota-akizuki
             </ExtLink>
           </li>
           <li>
-            Mail
+            <Envelope style={{ fill: 'var(--color-accents-1)', height: 24 }} />
             <ExtLink href="mailto:shota0609akizuki@gmail.com">
               {' '}
               shota0609akizuki@gmail.com
             </ExtLink>
           </li>
           <li>
-            Instagram
+            <Instagram style={{ fill: 'var(--color-accents-1)', height: 24 }} />
             <ExtLink href="https://instagram.com/shota_akizuki">
               {' '}
               @shota_akizuki
