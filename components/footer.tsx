53728b821ad704a3593aecbfb6571262168c08ec
shota-akizuki <shota0609akizuki@gmail.com>
2023-01-07 22:43:11 +0900

    add: 日付を動的に表示する

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,11 @@
 import styles from '../styles/footer.module.css';
 
 export const Footer = () => {
+  const now = new Date();
+  const year = now.getFullYear();
   return (
     <footer className={styles.footer}>
-      <span>2021 ©Shota Akizuki All rights reserved.</span>
+      <span>2021-{year} ©Shota Akizuki All rights reserved.</span>
     </footer>
   );
 };
